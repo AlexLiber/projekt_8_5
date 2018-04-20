44048083ac455f84a4510ad68f1a422afad1f350
AlexLiber <liberaleksa@gmail.com>
2018-04-21 01:21:58 +0200

    Correct intendation in html and spaces in js

--- triangleArea.js
+++ triangleArea.js
@@ -1,5 +1,5 @@
 var a = 9,
-	h = 7,
-	triangleArea = a*h/2;
+    h = 7,
+    triangleArea = a * h / 2;
 console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
 prompt('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
